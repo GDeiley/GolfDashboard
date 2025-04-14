58a44c297efb0acd0cd2630ae6853e1ce3fb5577
Grayson Deiley <Deiley921@gmail.com>
2025-04-14 12:20:57 -0400

    added cicd

--- src/app/clubs/page.jsx
+++ src/app/clubs/page.jsx
@@ -5,7 +5,7 @@ import Navbar from "@/components/ui/navbar"
 export default function Page() {
     return (
         <div>
-            <Navbar title="Clubs" />
+            <Navbar title="Clubss" />
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-6">
                 <Clubs />
             </div>
