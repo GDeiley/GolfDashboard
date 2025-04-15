0982dc0920f690cc22fba4b9361bd3316a06e222
Grayson Deiley <Deiley921@gmail.com>
2025-04-14 21:58:22 -0400

    added code

--- .next/static/chunks/src_9b4c05e8._.js
+++ .next/static/chunks/src_9b4c05e8._.js
@@ -406,7 +406,7 @@ function Dashboard() {
     return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
         children: [
             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$src$2f$components$2f$ui$2f$navbar$2e$jsx__$5b$app$2d$client$5d$__$28$ecmascript$29$__["default"], {
-                title: "Dashboardss"
+                title: "Dashboard Page"
             }, void 0, false, {
                 fileName: "[project]/src/app/page.jsx",
                 lineNumber: 10,
