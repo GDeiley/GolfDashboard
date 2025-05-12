01c2af7f88372556f6fee0d981b31c8b0c84c30b
Grayson Deiley <Deiley921@gmail.com>
2025-05-11 21:21:39 -0400

    adding to aws instance

--- src/app/clubs/page.jsx
+++ src/app/clubs/page.jsx
@@ -4,7 +4,7 @@ import DisplayClubs from "@/components/ui/displayclubs";
 export default function Page() {
     return (
         <div>
-            <Navbar title="Clubs In the Bag" />
+            <Navbar title="Clubs In Your Bag" />
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-6">
                 <DisplayClubs />
             </div>
